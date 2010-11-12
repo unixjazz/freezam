e2f2dca658036af61a095b365d22b93a75e44c93
unixjazz <unixjazz@gmail.com>
2010-11-11 16:54:03 -0800

    added logo.jpg to the project

--- gconvert.sh
+++ gconvert.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-if [ $@ -ne 2 ]; then echo "Gstreamer-base Audio Converter ANY-TO-WAV, usage: $0 arq1.ANY arq2.wav"; return 1; fi 
+if [ $@ -ne 2 ]; then echo "Gstreamer-base Audio Converter ANY-TO-WAV 0.1, usage: $0 arq1.ANY arq2.wav"; return 1; fi 
 gst-launch-0.10 filesrc location=$1 ! decodebin ! audioconvert ! audio/x-raw-int,channels=1 ! wavenc ! filesink location=$2
